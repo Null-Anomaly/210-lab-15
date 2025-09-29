3d07363e7ff38b39dfe25bf95c6fe053d4987c34
L B <lbryant019@insite.4cd.edu>
2025-09-28 17:14:54 -0700

    10 minutes

--- main.cpp
+++ main.cpp
@@ -3,10 +3,42 @@ IDE used: VSC*/
 
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 using namespace std;
+
+class Movie
+{
+    public:
+    string getWriter() {return writer;}
+    string getMovie() {return movie;}
+    int getYear() {return year;}
+    void setWriter(string w) {writer = w;}
+    void setMovie(string m) {movie = m;}
+    void setYear(int y) {year = y;}
+
+    private:
+    string writer;
+    string movie;
+    int year;
+
+};
 
 int main()
 {
+    string tempw;
+    string tempm;
+    int tempy;
+
+    ifstream fin("input.txt.txt");
+
+    if (fin.good())
+    {
+        while(fin >> tempw)
+
+
+        fin.close();
+    }
+
 
     return 0;
 }
